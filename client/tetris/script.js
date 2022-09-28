663442047de0661219f6e0e12a488da907d3eef1
okennedyumd <okennedy@umd.edu>
2022-09-28 16:28:54 -0400

    final tetris game

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -95,6 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
       draw();
       displayShape();
       addScore();
+      gameOver();
     }
   }
 
@@ -182,5 +183,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  function gameOver() {
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      scoreDisplay.innerHTML = 'end'
+      clearInterval(timerId)
+    }
+  }
 
 });
