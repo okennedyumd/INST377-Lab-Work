3ad006a3d0a463e111a0f31f61f337862ba8bed4
okennedyumd <okennedy@umd.edu>
2022-11-14 16:44:53 -0500

    map working

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -83,6 +83,20 @@ function filterList(array, filterInputValue) {
   return newArray;
 }
 
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.7849, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+return map;
+}
+
+function markerPlace() {
+  console.log('markerPlace', array);
+}
+
 async function mainEvent() {
   /*
         ## Main Event
@@ -90,7 +104,7 @@ async function mainEvent() {
           When you're not working in a heavily-commented "learning" file, this also is more legible
           If you separate your work, when one piece is complete, you can save it and trust it
       */
-
+  const pageMap = initMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
